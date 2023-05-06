f1354e5e20fb913542e33797c200c93fb310229a
Renat Notfullin <renat@notfullin.com>
2023-05-06 23:40:12 +0300

    Initial Commit

---
+++ linkq/linkqApp.swift
@@ -0,0 +1,17 @@
+//
+//  linkqApp.swift
+//  linkq
+//
+//  Created by Renat Notfullin on 06.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct linkqApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
